5de71dc5a365ea64bf7fc3106612f1ad2d016cad
m560h <914892930@qq.com>
2018-04-21 20:25:22 +0800

    imread and imwrite for different format pics

--- basic/basic_opencv.py
+++ basic/basic_opencv.py
@@ -10,4 +10,9 @@ cv2.imwrite('../picture/mountain1.jpg', img, [cv2.IMWRITE_JPEG_QUALITY, 50])
 # png format: lossless compression, compression ratio： 0 - 9
 cv2.imwrite('../picture/mountain1.png', img, [cv2.IMWRITE_PNG_COMPRESSION, 0])
 
-
+# manipulate pixels
+(b, g, r) = img[100, 100]
+print(b, g, r)
+for i in range(100):
+    img[i, 100] = (255, 255, 0)
+cv2.imwrite('../picture/mountain3.jpg', img)
