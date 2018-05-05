6b1932a72d348dcd2e14fd53d23505c77548e79e
m560h <914892930@qq.com>
2018-05-05 18:14:33 +0800

    resize pic using opencv

---
+++ opencv/resize_pic.py
@@ -0,0 +1,11 @@
+# resize pic using opencv
+import cv2
+
+img = cv2.imread('../picture/mountain.jpg', 1)
+shape = img.shape
+height = int(shape[0] * 0.5)
+width = int(shape[1] * 0.5)
+mode = shape[2]
+dst = cv2.resize(img, (height, width))
+cv2.imshow("dst", dst)
+cv2.waitKey(0)
