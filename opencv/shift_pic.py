aea75e892e8727625ef53cd65c8859bec80c9dd4
m560h <914892930@qq.com>
2018-05-06 15:38:34 +0800

    shift pic using opencv

---
+++ opencv/shift_pic.py
@@ -0,0 +1,16 @@
+# shift pic using opencv
+
+import numpy as np
+import  cv2
+
+img = cv2.imread('../picture/mountain.jpg', 1)
+info = img.shape
+height = info[0]
+width = info[1]
+mode = info[2]
+
+shiftMat = np.array([[1, 0, 100], [0, 1, 200]], dtype = np.float32) //shift mat
+dst = cv2.warpAffine(img, shiftMat, (height, width))
+
+cv2.imshow('dst', dst)
+cv2.waitKey(0)
