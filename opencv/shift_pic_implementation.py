8dbafe90a4518b8e7e17b5051dba61b08951261b
m560h <914892930@qq.com>
2018-05-06 15:38:56 +0800

    implementation of pic shift

---
+++ opencv/shift_pic_implementation.py
@@ -0,0 +1,18 @@
+# implementation of pic shift
+
+import cv2
+import numpy as np
+
+img = cv2.imread('../picture/mountain.jpg', 1)
+info = img.shape
+height = info[0]
+width = info[1]
+
+dst = np.zeros(info, np.uint8)
+
+for i in range(height):
+    for j in range(width - 100):
+        dst[i, j + 100] = img[i, j]
+
+cv2.imshow('dst', dst)
+cv2.waitKey(0)
