5225c26b8937882b391fb5db5013cb7a391252bd
m560h <914892930@qq.com>
2018-05-12 20:43:57 +0800

    rotate pic using opencv

---
+++ opencv/rotate_pic.py
@@ -0,0 +1,18 @@
+# rotate pic using opencv
+
+import cv2
+import numpy as np
+
+img = cv2.imread('../picture/opencv.jpg', 1)
+info = img.shape
+height = info[0]
+width = info[1]
+mode = info[2]
+cv2.imshow('src', img)
+
+
+matRotate = cv2.getRotationMatrix2D((height / 2, width / 2), 45, 0.4)
+dst = cv2.warpAffine(img, matRotate, (width, height))
+
+cv2.imshow('dst', dst)
+cv2.waitKey(0)
