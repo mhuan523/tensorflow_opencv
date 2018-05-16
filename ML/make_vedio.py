1849408762452629c99a5aad7ca3f2f4ce501fe2
m560h <914892930@qq.com>
2018-05-16 12:36:00 +0800

    make vedio using opencv

---
+++ ML/make_vedio.py
@@ -0,0 +1,15 @@
+# make vedio using opencv
+
+import cv2
+
+img = cv2.imread('../vedio/image_1.jpg', 1)
+info = img.shape
+
+size = (info[1], info[0])
+
+vedio_writer = cv2.VideoWriter('../vedio/2.mp4', -1, 15, size)
+
+for i in range(1, 11):
+    file_name = 'image_' + str(i) + '.jpg'
+    frame = cv2.imread('../vedio/' + file_name, 1)
+    vedio_writer.write(frame)
